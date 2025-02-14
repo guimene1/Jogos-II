7560a267c36ef488754c9b9d04aa612abc31e899
guimene1 <141081187+guimene1@users.noreply.github.com>
2025-02-14 20:23:24 -0300

    dai 1

---
+++ Assets/Aula/Interactable2.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Interactable2 : MonoBehaviour
+{
+    public string customMessage;
+
+    public string GetInteractionMessage()
+    {
+        return string.IsNullOrEmpty(customMessage) ? "Interagir com" + gameObject.name : customMessage;
+    }
+    public void Interact()
+    {
+        Debug.Log($"Interagiu com {gameObject.name}");
+    }
+}
